382ccf34fc6df7ef9b64cd53466a863e0d2c2572
anthonyjspoerl <spoerla@uni.edu>
2013-09-24 18:14:32 -0500

    Scanning and separating tokens now working
    I basically redid the scanner we had entirely with this, mostly as that
    work is being moved to the Token class as I understand it. I forgot to
    add a few functions we will need like peek(), but it's doing relatively
    what it should.

--- Scanner.java
+++ Scanner.java
@@ -1,67 +1,101 @@
-
-
+import java.io.PushbackInputStream;
+import java.io.IOException;
+import java.io.FileInputStream;
 
 public class Scanner
 {
-    public static void main( String[] args )
-    {
-        int START_STATE    = 0;
-        int TERMINAL_STATE = 4; 
-        
+  private PushbackInputStream sourceFile;
+  private Token               nextToken; // Added this for peek as Wallingford pointed out we would need it
 
-        String candidate = "main";
-        char   next;
-        
-        if (candidate.length()!=4){
-        	System.out.println("Reject");
-        	System.exit(0);
-        }
+  // Kept here for testing purposes
+  public static void main( String[] args ) throws java.io.FileNotFoundException, IOException
+  {
+    try{
+    PushbackInputStream input = new PushbackInputStream(new FileInputStream("test.txt"));
 
-        int state = START_STATE;
-        for (int i = 0; i < candidate.length(); i++)
-        {
-            next = candidate.charAt(i);
-            switch (state)
-            {
-                case 0:
-                    switch ( next )
-                    {
-                        case 'm': state++; break;
-                        default : state = -1;
-                    }
-                    break;
-
-                case 1:
-                    switch ( next )
-                    {
-                        case 'a': state++; break;
-                        default : state = -1;
-                    }
-                    break;
-
-                case 2:
-                    switch ( next )
-                    {
-                        case 'i': state++; break;
-                        default : state = -1;
-                    }
-                    break;
-
-                case 3:
-                    switch ( next )
-                    {
-                        case 'n': state++; break;
-                        default : state = -1;
-                    }
-                    break;
-                  
-            }
-        }
-
-        if ( state == TERMINAL_STATE )
-            System.out.println( "accept" );
-        else
-            System.out.println( "reject" );
+    Scanner test = new Scanner( input );
+    
+    String tmp = test.getNextToken();
+    while( tmp != "" ){
+      System.out.println(tmp);
+      tmp = test.getNextToken();
     }
-}
+    
+    }
+    catch(Exception e){
+      System.out.println( e );
+    }
+  }
 
+  public Scanner( PushbackInputStream input )
+  {
+    sourceFile = input;
+    nextToken = null;
+  }
+  
+  // Was having problems getting the Token class to work here so for now it just keeps everything as a string.
+  // Ideally we will return a Token from here which will be identified in its constructor.
+  public String getNextToken() throws IOException
+  {
+    String rawToken = "";
+    int nextByte = getNextByte();
+    
+    if( isSymbol( (char) nextByte) ){
+      return Character.toString((char) nextByte);
+    }
+
+    while( !isSymbol((char) nextByte) ){
+      if( nextByte != -1 ){ // EOF character
+        rawToken += (char) nextByte;
+        nextByte = getNextByte();
+      }
+      else{
+        return rawToken;
+      }
+    }
+    
+    // If we reach here we found a symbol signifying a new token, so we must first replace it in the stream
+    sourceFile.unread( nextByte );
+    return rawToken;
+  }
+  
+  // This is reading bytes... just not the right ones? 
+  public int getNextByte() throws IOException
+  {
+    int nextChar;
+    while(true)
+    {
+      nextChar = sourceFile.read();
+      if( nextChar == -1 )
+        return -1;
+      if( !isWhitespace((char) nextChar) )
+        return nextChar;
+    }
+  }
+  
+  // Static member funtions
+  public static boolean isWhitespace( char c )
+  {
+    return c == ' '  ||
+      c == '\b' ||
+      c == '\f' ||
+      c == '\n' ||
+      c == '\r' ||
+      c == '\t';
+  }
+  
+  public static boolean isSymbol( char c )
+  {
+    return c == '+' ||
+      c == '-' ||
+      c == '*' ||
+      c == '/' ||
+      c == '<' ||
+      c == '=' ||
+      c == '(' ||
+      c == ')' ||
+      c == ',' ||
+      c == ':';
+    // Note: Comment tag is two characters and is perceived as a keyword
+  }
+}
