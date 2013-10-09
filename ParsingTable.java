c25ec2bf9061fcf901745a1dab838c76ee93466f
jordangreiner <jgreiner@uni.edu>
2013-10-08 22:32:34 -0500

    convertNonTerminal function update
    Did it for OUR grammar but realized it doesn't make much sense with  our token class. I'm sure we'll figure it out tomorrow...

--- ParsingTable.java
+++ ParsingTable.java
@@ -36,14 +36,36 @@ public class ParsingTable {
 
     private int convertNonTerminal( Token symbol )
     {
-        if (symbol.getValue().equals( "program" )       ) return 0;
-        if (symbol.equals( NonTerminal.Declarations )  ) return 1;
-        if (symbol.equals( NonTerminal.Declaration )   ) return 2;
-        if (symbol.equals( NonTerminal.Statements )    ) return 3;
-        if (symbol.equals( NonTerminal.Statement )     ) return 4;
-        if (symbol.equals( NonTerminal.ExpressionTail )) return 5;
-        if (symbol.equals( NonTerminal.Value )         ) return 6;
-
+        if (symbol.getValue().equals( "program" )          ) return 0;
+        if (symbol.getValue().equals( "definitions" )      ) return 0;
+        if (symbol.getValue().equals( "def1" )             ) return 0;
+        if (symbol.getValue().equals( "definitions1" )     ) return 0;
+        if (symbol.getValue().equals( "def" )              ) return 0;
+        if (symbol.getValue().equals( "formals" )          ) return 0;
+        if (symbol.getValue().equals( "nonemptyformals" )  ) return 0;
+        if (symbol.getValue().equals( "nonemptyformals1" ) ) return 0;
+        if (symbol.getValue().equals( "formal" )           ) return 0;
+        if (symbol.getValue().equals( "body" )             ) return 0;
+        if (symbol.getValue().equals( "body1" )            ) return 0;
+        if (symbol.getValue().equals( "type" )             ) return 0;
+        if (symbol.getValue().equals( "expr" )             ) return 0;
+        if (symbol.getValue().equals( "simple-expr1" )     ) return 0;
+        if (symbol.getValue().equals( "simple-expr" )      ) return 0;
+        if (symbol.getValue().equals( "term2" )            ) return 0;
+        if (symbol.getValue().equals( "term" )             ) return 0;
+        if (symbol.getValue().equals( "term1" )            ) return 0;
+        if (symbol.getValue().equals( "factor" )           ) return 0;
+        if (symbol.getValue().equals( "identifier1" )      ) return 0;
+        if (symbol.getValue().equals( "actuals1" )         ) return 0;
+        if (symbol.getValue().equals( "actuals" )          ) return 0;
+        if (symbol.getValue().equals( "nonemptyactuals" )  ) return 0;
+        if (symbol.getValue().equals( "nonemptyactuals1" ) ) return 0;
+        if (symbol.getValue().equals( "literal" )          ) return 0;
+        if (symbol.getValue().equals( "print" )            ) return 0;       
+        
+        //left all numbers as 0 until we figure out what were keeping
+        //hey group...should identifier be one? it's not in our grammer
+        
         return 0; // return an exception
     }
 
