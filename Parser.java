4b3fdf940f42ddf7acc267157ea73d1a7ad9f68d
anthonyjspoerl <spoerla@uni.edu>
2013-10-10 23:02:58 -0500

    Revert "Added Parser.java so TDP will run; Removed junk"
    This reverts commit da8337ebae46e60270a627ef894e85046bc17b83.

--- Parser.java
+++
@@ -1,8 +0,0 @@
-public class Parser{
-  protected Scanner scanner;
-  
-  public Parser( Scanner scanner )
-  {
-    this.scanner = scanner;
-  }
-}
--- PushSequence.java
+++ PushSequence.java
@@ -1,26 +1,26 @@
 import java.util.Stack;
 
 public class PushSequence implements ParseAction {
+	
+	   private ParseAction[] actions;
 
-  private ParseAction[] actions;
+	   public PushSequence( ParseAction[] actions )
+	   {
+	      this.actions = actions;
+	   }
 
-  public PushSequence( ParseAction[] actions )
-  {
-    this.actions = actions;
-  }
-  
-  public void execute( Stack stack )
-  {
-    for (int i = actions.length-1; i >= 0; i--)
-      actions[i].execute( stack );
-  }
-  
-  public String toString()
-  {
-    String answer = "";
-    for (int i = 0; i < actions.length; i++)
-      answer += (actions[i].toString() + ", ");
-    return answer;
-  }
-  
+	   public void execute( Stack stack )
+	   {
+	      for (int i = actions.length-1; i >= 0; i--)
+	          actions[i].execute( stack );
+	   }
+
+	   public String toString()
+	   {
+	      String answer = "";
+	      for (int i = 0; i < actions.length; i++)
+	          answer += (actions[i].toString() + ", ");
+	      return answer;
+	   }
+
 }
