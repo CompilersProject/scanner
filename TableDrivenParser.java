47d0d1551a55c5881819a4625a33985ac466989e
anthonyjspoerl <spoerla@uni.edu>
2013-10-10 20:48:51 -0500

    Algorithm, mostly working

--- TableDrivenParser.java
+++ TableDrivenParser.java
@@ -4,8 +4,8 @@ import java.util.Stack;
 public class TableDrivenParser extends Parser
 
 {
-	public Stack stackAttack;
-    private ParsingTable kleinTable;
+  public Stack stackAttack;
+  private ParsingTable kleinTable;
 
     public TableDrivenParser( Scanner source )
     {
@@ -13,14 +13,43 @@ public class TableDrivenParser extends Parser
         kleinTable = makeKleinParsingTable();
     }
 
-    protected void parseProgram() throws 
-                                            IOException
+    protected void parseProgram() throws IOException, LexicalException
     {
-    	
-    	
+      Stack parseStack = new Stack();
+      
+      new Push( new Token(Token.TYPE.EOS) ).execute(parseStack);   // Step 1
+      new Push( "PROGRAM" ).execute(parseStack); // Step 2
 
-        // ... the algorithm
-        // return answer;
+      while( true ){ // Step 3?
+        Object symbol = parseStack.pop(); // Pop A
+        
+        if( symbol instanceof Token ){ // A is terminal
+          Token terminal = (Token) symbol;
+          
+          if( terminal.getType() == Token.TYPE.EOS ){
+            System.out.println("LE FIN");
+            return;
+          } else if( terminal.getType() == Token.TYPE.COMMENT ){
+            break; // Ignore comments
+          } else if( terminal.equals( scanner.peek() )){
+            scanner.getNextToken();
+          } else{
+            throw new LexicalException("Terminal Error: Make a new exception for this, stupid");
+          }
+        }
+        else { // A is non-terminal
+          String nonTerminal = (String) symbol;
+          ParseAction rule = kleinTable.lookup(nonTerminal,scanner.peek());
+          
+          if( rule != null ){ // TODO: Does this work?
+            new PushSequence( rule ).execute(parseStack); // TODO: Loop through and push these on backwards 
+          } else{ // Failed to find rule for table[A,i]
+            throw new LexicalException("Non-Terminal Error: Make a new exception for this, stupid");
+          }
+        }
+      }
+      // ... the algorithm
+      // return answer;
     }
 
     // -------------------------------------------------------------------
@@ -31,7 +60,7 @@ public class TableDrivenParser extends Parser
        ParsingTable table = new ParsingTable();
 
        try {
-    	   
+/*
        Token endOfStream        = new Token("$");
        Token plusOp             = new Token("+");
        Token minusOp            = new Token("-");
@@ -58,14 +87,14 @@ public class TableDrivenParser extends Parser
        Token integerOp          = new Token("integer");
        Token printOp            = new Token("print");
        Token identifierOp       = new Token("identifier");
-       Token print				= new Token("print");
+       Token print    = new Token("print");
 
        
 
        ParseAction rule00 = new PushNothing();
        ParseAction ruleProgram = new PushSequence(
                  new ParseAction[] { new Push("DEFINITIONS"),
-                                	 new Push(endOfStream)
+                                  new Push(endOfStream)
                       } );
        ParseAction ruleDefinitions = new PushSequence(
                  new ParseAction[] { new Push("DEF1")
@@ -73,15 +102,15 @@ public class TableDrivenParser extends Parser
                       } );
        ParseAction ruleDef1 = new PushSequence(
                  new ParseAction[] { new Push("DEF"),
-                		 			 new Push("DEFINITIONS1")
+                       new Push("DEFINITIONS1")
                                      
                       } );
        ParseAction ruleDefinitions1 = new PushSequence(
                  new ParseAction[] {  new Push("Definitions"),
-                		              
+                                
                       } );
        ParseAction ruleDef = new PushSequence(
-               	new ParseAction[] { new Push("IDENTIFIER"),
+                new ParseAction[] { new Push("IDENTIFIER"),
                                     new Push(openParen),
                                     new Push("FORMALS"),
                                     new Push(closedParen),
@@ -131,7 +160,7 @@ public class TableDrivenParser extends Parser
                     } );
        ParseAction ruleSimpleExpr1Part2 = new PushSequence(
                new ParseAction[] { new Push(assignmentOp),
-            		   		       new Push("EXPR"),
+                          new Push("EXPR"),
                     } );
        ParseAction ruleSimpleExpr = new PushSequence(
                new ParseAction[] { new Push("TERM"),
@@ -202,21 +231,21 @@ public class TableDrivenParser extends Parser
                     } );
        ParseAction ruleNonEmptyActuals = new PushSequence(
                new ParseAction[] { new Push("EXPR"),
-                              	   new Push(comma),
-                              	   new Push("NONEMPTYACTUALS1")
+                                  new Push(comma),
+                                  new Push("NONEMPTYACTUALS1")
                     } );
        ParseAction ruleNonEmptyActuals1 = new PushSequence(
                new ParseAction[] { new Push("NONEMPTYACTUALS")
                     } );
        ParseAction ruleLiteral = new PushSequence(
                new ParseAction[] { new Push("NUMBER"),
-            		   	           new Push("BOOLEAN"),
+                             new Push("BOOLEAN"),
                     } );
        ParseAction rulePrint = new PushSequence(
                new ParseAction[] { new Push(print),
-            		   			   new Push(openParen),
-            		   			   new Push("EXPR"),
-            		   			   new Push(closedParen)
+                       new Push(openParen),
+                       new Push("EXPR"),
+                       new Push(closedParen)
                     } );
 
 
@@ -251,7 +280,7 @@ public class TableDrivenParser extends Parser
        table.add( NonTerminal.Value, identifier,   rule10 );
        table.add( NonTerminal.Value, floatValue,   rule11 );
        table.add( NonTerminal.Value, integerValue, rule12 );
-
+*/
        } catch (Exception e) {
            System.out.println( "Trouble building parse table.  No entries." );
        }
